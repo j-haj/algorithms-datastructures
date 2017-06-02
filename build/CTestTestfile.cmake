b66e692403c511cf1ab40f4782a989443e28da56
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-06-02 06:24:07 -0500

    Removed build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jhaj/src/github/algorithms-datastructures
-# Build directory: /Users/jhaj/src/github/algorithms-datastructures/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
