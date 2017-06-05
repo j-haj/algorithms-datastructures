8e0a9c3393352414f6dc16f12710fdcb8334b5db
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-06-05 08:02:22 -0500

    Adds a number of Vector tests

--- test/containers/vector_test.h
+++ test/containers/vector_test.h
@@ -1,7 +1,14 @@
+#include <random>
+
 #include "containers/vector.h"
 
 namespace vector_test {
   const int kRepeatNum = 100;
+  const int kMaxRange = 100;
+
+  std::random_device r;
+  std::default_random_engine e(r());
+  std::uniform_int_distribution<int> dist(1, kMaxRange);
 }
 TEST(VectorTest, EmptyConstruction) {
   auto v = containers::Vector<int>();
@@ -11,7 +18,34 @@ TEST(VectorTest, EmptyConstruction) {
 
 TEST(VectorTest, SizeConstructor) {
   for (int i = 0; i < vector_test::kRepeatNum; ++i) {
-    auto v = containers::Vector<int>(i);
-    ASSERT_EQ(v.size(), i);
+    int size = vector_test::dist();
+    auto v = containers::Vector<int>(size);
+    ASSERT_EQ(v.size(), size);
+  }
+}
+
+TEST(VectorTest, ListConstructor) {
+  containers::Vector<int> v({1, 2, 3, 4});
+  ASSERT_EQ(v.size(), 4);
+  for (int i = 0; i < v.size(); ++i) {
+    ASSERT_EQ(v[i], i+1);
+  }
+}
+
+TEST(VectorTest, MoveConstructor) {
+  auto v = containers::Vector<int> ({1, 2, 3, 4});
+  ASSERT_EQ(v.size(), 4);
+  for (int i = 0; i < v.size(); ++i) {
+    ASSERT_EQ(v[i], i+1);
+  }
+}
+
+TEST(VectorTest, CopyConstructor) {
+  auto v1 = containers::Vector<int>({1, 2, 3, 4});
+  auto v2 = v1;
+  ASSERT_EQ(v1.size(), v2.size());
+  ASSERT_EQ(v1.capacity(), v2.capacity());
+  for (int i = 0; i < v1.size(); ++i) {
+    ASSERT_EQ(v1[i], v2[i]);
   }
 }
