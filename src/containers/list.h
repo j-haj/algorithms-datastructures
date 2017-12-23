1f1f0c35b282ca165e760608d1a11c6cef6fabe8
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-12-23 16:21:56 -0600

    gets gtest running for list

--- src/containers/list.h
+++ src/containers/list.h
@@ -12,6 +12,10 @@ struct Node {
     next.reset();
   }
 
+  Node& operator=(Node& n) {
+    return n;
+  }
+
   /// Holds the data for the node
   T value;
 
@@ -19,23 +23,25 @@ struct Node {
   NodePtr next;
 };
 
-std::ostream& operator<<(std::ostream& os, Node& o) {
+template <typename T>
+std::ostream& operator<<(std::ostream& os, Node<T>& o) {
   os << "Node< " << o.value << ", " << o.next.get() << ">";
   return os;
 }
 
 template <typename T>
 class ForwardList {
-  using NodePtr = std::make_unique<Node<T>>;
+  using NodePtr = std::unique_ptr<Node<T>>;
   public:
     void insert(T v) {
       auto tmp = std::make_unique<Node<T>>(v);
       tmp->next.reset(head_.release());
-      head_.reset(tmp);
+      head_.reset(tmp.release());
     }
 
     size_t size() const noexcept { return size_; }
 
+    Node<T>* head() { return head_.get(); }
   private:
     /// Number of nodes in the list
     size_t size_;
@@ -44,5 +50,13 @@ class ForwardList {
     NodePtr head_;
 
 }; // class ForwardList
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, ForwardList<T>& o) {
+  Node<T>* p = o.head();
+  while(p->next != nullptr) {
+    os << p->value << ' ';
+  }
+}
 
 #endif // LIST_H_
